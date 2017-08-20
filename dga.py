220616ee39706a772325b5c47da231b293ce23a3
Josh Horowitz <joshua.m.horowitz@gmail.com>
2017-08-20 12:36:50 -0400

    init commit

---
+++ dga.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from requests import get as http_get
+from random import choice
+import hashlib
+
+
+def get_default_word_list():
+    url = "https://raw.githubusercontent.com/paritytech/wordlist/master/res/wordlist.txt"
+    expected_hash = "6d557f0693958fb5e650b68b5bee585eb82cf4da32965505c789e924743bc522"
+    return get_word_list(url, expected_hash)
+
+
+def get_word_list(url, expected_hash, minimum_word_length=4):
+    text = http_get(url).text
+    hashed = hashlib.sha256(text).hexdigest()
+
+    assert hashed == expected_hash, "Unexpected hash: " + hashed + "\nResponse Text Truncated:\n" + text[:100]
+
+    return [x for x in text.split("\n") if len(x) >= minimum_word_length]
+
+
+def generate_domain(word_list, tld, words_per_domain=3):
+    return "".join([choice(word_list) for _ in xrange(words_per_domain)]) + tld
+
+
+def generate_domains(word_list, tld, domain_count, words_per_domain=3):
+    return [generate_domain(word_list, tld, words_per_domain) for _ in xrange(domain_count)]
+
+
+def main(tld, domain_count, word_list):
+    generated = generate_domains(word_list, tld, domain_count)
+
+    for v in generated:
+        print v
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 3:
+        print "Usage: python dga.py <TLD> <domain_count>"
+        sys.exit(0)
+
+    tld = sys.argv[1]
+    if tld[0] != '.':
+        tld = '.' + tld
+
+    domain_count = int(sys.argv[2])
+
+    main(tld, domain_count, get_default_word_list())
